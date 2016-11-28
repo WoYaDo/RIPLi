d6bef338b15c360d319f4cd5924c56da3246963f
Yannick <yannick-verschueren1@hotmail.com>
2016-11-28 12:41:12 +0100

    Fitlers en stuff

--- PythonImageEditor/main.py
+++ PythonImageEditor/main.py
@@ -3,6 +3,8 @@ import tkFileDialog as filedialog
 import PIL
 from PIL import Image
 from PIL import ImageTk
+from PIL import ImageEnhance
+from functools import partial
 import glob
 import os
 
@@ -35,7 +37,7 @@ def leftkey(event):
     elif currentmode == 'edit':
         editmenubar.entryconfig(editindex,background="lightgrey")
         editindex = editindex - 1
-        rootindex = max(1,editindex)
+        rootindex = max(6,editindex)
         editmenubar.entryconfig(editindex,background="red")
     else:
         print currentmode
@@ -52,7 +54,7 @@ def rightkey(event):
     elif currentmode == 'edit':
         editmenubar.entryconfig(editindex,background="lightgrey")
         editindex = editindex + 1
-        rootindex = min(4,editindex)
+        rootindex = min(6,editindex)
         editmenubar.entryconfig(editindex,background="red")
     else:
         print currentmode
@@ -73,7 +75,9 @@ imageframe = Frame(root,height=200,width=300)
 editmenubar = Menu(root)
 editmenubar.add_command(label="Rotate")
 editmenubar.add_command(label="Scale")
-editmenubar.add_command(label="Choose Image")
+editmenubar.add_command(label="Brightness")
+editmenubar.add_command(label="Contrast")
+editmenubar.add_command(label="Filter")
 editmenubar.add_command(label="Close")
 
 def enterEditMode():
@@ -86,8 +90,10 @@ def enterEditMode():
 
     editmenubar.entryconfig(1, command=rotate)
     editmenubar.entryconfig(2, command=scale)
-    editmenubar.entryconfig(3, command=chooseimage)
-    editmenubar.entryconfig(4, command=closeedit)
+    editmenubar.entryconfig(3, command=brightness)
+    editmenubar.entryconfig(4, command=contrast)
+    editmenubar.entryconfig(5, command=ifilter)
+    editmenubar.entryconfig(6, command=closeedit)
 
     editmenubar.entryconfig(1,background="red")
 
@@ -123,7 +129,7 @@ def openfilemenu():
     COLUMNS = 10
     image_count = 0
 
-    filenames = glob.glob(os.path.join(os.getcwd(), '*.gif'))
+    filenames = glob.glob(os.path.join(os.getcwd(), '*[!.py]'))
     labels = []
 
     def left(event):
@@ -232,6 +238,7 @@ def rotate():
     setKey("right",rotate_right)
     setKey("back",back)
 
+#######################################################
 
 def scale_left(event):
     label = imageframe.winfo_children()[0]
@@ -262,6 +269,146 @@ def scale():
     setKey("right",scale_right)
     setKey("back",back)
 
+###############################################
+def brightness_left(event):
+    label = imageframe.winfo_children()[0]
+    oldbrightness = label.brightness
+    newbrightness = oldbrightness - 0.1
+    enhancer = ImageEnhance.Brightness(label.image.original)
+    bright_im = enhancer.enhance(newbrightness) #any value you want
+    newimage = bright_im
+    newphimage = ImageTk.PhotoImage(newimage)
+    newphimage.current = newimage
+    newphimage.original = label.image.original
+    newphimage.rotation = label.image.rotation
+    label.configure(image=newphimage)
+    label.image=newphimage
+    label.brightness = newbrightness
+
+def brightness_right(event):
+    label = imageframe.winfo_children()[0]
+    oldbrightness = label.brightness
+    newbrightness = oldbrightness + 0.1
+    enhancer = ImageEnhance.Brightness(label.image.original)
+    bright_im = enhancer.enhance(newbrightness) #any value you want
+    newimage = bright_im
+    newphimage = ImageTk.PhotoImage(newimage)
+    newphimage.current = newimage
+    newphimage.original = label.image.original
+    newphimage.rotation = label.image.rotation
+    label.configure(image=newphimage)
+    label.image=newphimage
+    label.brightness = newbrightness
+
+def brightness():
+    label = imageframe.winfo_children()[0]
+    label.brightness = 1.0
+    setKey("left", brightness_left)
+    setKey("right",brightness_right)
+    setKey("back",back)
+
+###############################################
+def contrast_left(event):
+    label = imageframe.winfo_children()[0]
+    oldcontrast = label.contrast
+    newcontrast = oldcontrast - 0.1
+    enhancer = ImageEnhance.Contrast(label.image.original)
+    bright_im = enhancer.enhance(newcontrast) #any value you want
+    newimage = bright_im
+    newphimage = ImageTk.PhotoImage(newimage)
+    newphimage.current = newimage
+    newphimage.original = label.image.original
+    newphimage.rotation = label.image.rotation
+    label.configure(image=newphimage)
+    label.image=newphimage
+    label.contrast = newcontrast
+
+def contrast_right(event):
+    label = imageframe.winfo_children()[0]
+    oldcontrast = label.contrast
+    newcontrast = oldcontrast + 0.1
+    enhancer = ImageEnhance.Contrast(label.image.original)
+    bright_im = enhancer.enhance(newcontrast) #any value you want
+    newimage = bright_im
+    newphimage = ImageTk.PhotoImage(newimage)
+    newphimage.current = newimage
+    newphimage.original = label.image.original
+    newphimage.rotation = label.image.rotation
+    label.configure(image=newphimage)
+    label.image=newphimage
+    label.contrast = newcontrast
+
+def contrast():
+    label = imageframe.winfo_children()[0]
+    label.contrast = 1.0
+    setKey("left", contrast_left)
+    setKey("right",contrast_right)
+    setKey("back",back)
+
+###############################################
+def filter1(image):
+    im = image.convert("L")
+    return im
+
+def filter2(image):
+    def make_linear_ramp(white):
+        ramp = []
+        r, g, b = white
+        for i in range(255):
+            ramp.extend((r*i/255, g*i/255, b*i/255))
+        return ramp
+
+    sepia = make_linear_ramp((255, 240, 192))
+    im = image.convert("L")
+    im.putpalette(sepia)
+    return im
+
+def filter0(image):
+    return image
+
+filters = [filter0,filter1,filter2] 
+
+def filter_right(event):
+    label = imageframe.winfo_children()[0]
+    curfilter = label.filter
+    newfilter = min(curfilter + 1,len(filters) - 1)
+    label = imageframe.winfo_children()[0]
+
+    origimage = label.image.original
+    filterfunction = filters[newfilter]
+    newimage = filterfunction(origimage)
+    newphimage = ImageTk.PhotoImage(newimage)
+    newphimage.current = newimage
+    newphimage.original = label.image.original
+    newphimage.rotation = label.image.rotation
+    label.configure(image=newphimage)
+    label.image=newphimage
+    label.filter = newfilter
+    
+def filter_left(event):
+    label = imageframe.winfo_children()[0]
+    curfilter = label.filter
+    newfilter = max(0,curfilter - 1)
+    label = imageframe.winfo_children()[0]
+
+    origimage = label.image.original
+    filterfunction = filters[newfilter]
+    newimage = filterfunction(origimage)
+    newphimage = ImageTk.PhotoImage(newimage)
+    newphimage.current = newimage
+    newphimage.original = label.image.original
+    newphimage.rotation = label.image.rotation
+    label.configure(image=newphimage)
+    label.image=newphimage
+    label.filter = newfilter
+
+def ifilter():
+    label = imageframe.winfo_children()[0]
+    label.filter = 0
+    setKey("left", filter_left)
+    setKey("right",filter_right)
+    setKey("back",back)
+################################################
 def chooseimage():
     return 0
 def closeedit():
